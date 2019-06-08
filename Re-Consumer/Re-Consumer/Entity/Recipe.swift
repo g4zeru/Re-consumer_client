1e9d19d103a441e14f436382de53d7c42992a37e
g4zeru <haru25haru02@gmail.com>
2019-06-08 16:01:05 +0900

    add entity

---
+++ Re-Consumer/Re-Consumer/Entity/Recipe.swift
@@ -0,0 +1,14 @@
+//
+//  Recipe.swift
+//  Re-Consumer
+//
+//  Created by iniad on 2019/06/08.
+//  Copyright © 2019 harutaYamada. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe: Codable {
+    let name: String
+    let url: URL
+}
